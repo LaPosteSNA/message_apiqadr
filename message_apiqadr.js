3f92e720928f1fd7c98d0c86f521826f13d00380
LUDO33500 <ludovic.pigeon@laposte.fr>
2018-12-12 14:07:21 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,2 +1,5 @@
-var welcome_page_messages = []   
+var welcome_page_messages = [
+  { "date" : "12/12/2018",
+    "message" : "une mise en production est prévue ce mercredi 12 décembre 2018 après-midi, de ce fait des impacts sur les services sont à prévoir durant cette période. Désolé pour la gêne occasionnée. Cordialement."
+  }]     
    
