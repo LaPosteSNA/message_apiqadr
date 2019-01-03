9770e70a2c6b2dca9095f06e676c7f1a58924973
Claude Seguret <clodio@users.noreply.github.com>
2019-01-03 11:15:17 +0100

    correction message

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,2 +1,2 @@
-var welcome_page_messages = [  { "date" : "02/01/2019",    "message" : "Notre service d'envoi de mail a quelques indisponibilités, il est possible que les mails arrivent avec du retard. Désolé pour la gêne occasionnée pour ce début d'année. Cordialement. Bonne année 2019."]   
+var welcome_page_messages = [  { "date" : "02/01/2019",    "message" : "Notre service d'envoi de mail a quelques indisponibilités, il est possible que les mails arrivent avec du retard. Désolé pour la gêne occasionnée pour ce début d'année. Cordialement. Bonne année 2019."}]   
    
