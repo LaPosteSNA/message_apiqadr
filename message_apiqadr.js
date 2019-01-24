6a4b7bba95e6f553514df8bac172aa811ecb5056
Claude Seguret <clodio@users.noreply.github.com>
2019-01-24 11:55:59 +0100

    suppression des messages

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,2 +1,2 @@
-var welcome_page_messages = [  { "date" : "24/01/2019",    "message" : "Une opération de mise à jour a lieu aujourd'hui, le service peut être perturbé."}]   
+var welcome_page_messages = [  ]   
    
