09d060263aa648076c5daf235b412bd0a35ea1e5
Claude Seguret <clodio@users.noreply.github.com>
2019-02-21 07:34:08 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,1 +1,1 @@
-var welcome_page_messages = [  { "date" : "21/02/2019",    "message" : "Une opération de maintenance est en cours ce jour. Le service peut être &ventuellement être perturbé."}]   
+var welcome_page_messages = [  { "date" : "21/02/2019",    "message" : "Une opération de maintenance est en cours ce jour. Le service peut être éventuellement être perturbé."}]   
