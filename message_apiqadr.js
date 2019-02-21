d09faa9afadd3acf516d836c5de73509944a1877
Claude Seguret <clodio@users.noreply.github.com>
2019-02-21 07:33:32 +0100

    ajout message mep

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,1 +1,1 @@
-var welcome_page_messages = [ ]   
+var welcome_page_messages = [  { "date" : "21/02/2019",    "message" : "Une opération de maintenance est en cours ce jour. Le service peut être &ventuellement être perturbé."}]   
