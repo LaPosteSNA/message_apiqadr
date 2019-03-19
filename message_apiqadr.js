484caff28da3a78b5610e966f9fd918b87b12a23
Claude Seguret <clodio@users.noreply.github.com>
2019-03-19 13:20:46 +0100

    Suppression message accueil

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,2 +1,2 @@
  
-var welcome_page_messages = [  { "date" : "14/03/2019",    "message" : "Suite à l'opération du 14/03, certains fichiers de résultat de traitement ont été effacés, nous pouvons à la demande vous les régénérer si besoin, contactez-nous à l'adresse mail ld-bscc-service-qualification-adresse@laposte.fr. Désolé pour la gêne occasionnée. "}]   
+var welcome_page_messages = [  ]   
