1dfa7ec417dd0c76841d6f34f57e3ab872099fc9
LUDO33500 <ludovic.pigeon@laposte.fr>
2019-06-06 16:45:34 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,7 +1,4 @@
  
 var welcome_page_messages = [
-  { "date" : "06/06/2019",
-    "message" : "une mise en production est prévue ce jeudi 09 juin 2019, de ce fait des impacts sur les services sont à prévoir durant cette période. Désolé pour la gêne occasionnée. Cordialement."
-  }
-   ]   
+     ]   
   
