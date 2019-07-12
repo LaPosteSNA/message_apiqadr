7126fcb50787e3d5a5336bde603402f11c250ed8
Claude Seguret <clodio@users.noreply.github.com>
2019-07-12 10:29:39 +0200

    Suppression message

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,7 +1,3 @@
  
-var welcome_page_messages = [
- { "date" : "11/07/2019",
-    "message" : "une mise en production est prévue ce jeudi 11 juillet 2019, de ce fait des impacts sur les services sont à prévoir durant cette période. Désolé pour la gêne occasionnée. Cordialement."
-  }
-     ]   
+var welcome_page_messages = []   
   
