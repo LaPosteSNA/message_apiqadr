4b7db470e1e5531ecf613c155d1b8832d9aaa5c8
Claude Seguret <clodio@users.noreply.github.com>
2019-08-22 15:57:19 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,5 +1,3 @@
  
-var welcome_page_messages = [{ "date" : "22/08/2019",
-    "message" : "Une opération de maintenance est prévue ce jour de 14h00 à 15h50. Le service peut être interrompu quelques minutes pendant cette opération. Désolé pour la gêne occasionnée. Cordialement."
-  }]   
+var welcome_page_messages = []   
   
