150b878faf74fab62eb306f5b02c3fbea2ff1244
Claude Seguret <clodio@users.noreply.github.com>
2020-01-06 12:07:30 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,3 +1,7 @@
  
 var welcome_page_messages = []   
+
+var welcome_page_messages = [{ "date" : "06/01/2020",
+    "message" : "Le service est temporairement indisponible, nous essayons de relancer le service dès que possible. Désolé pour la gêne occasionnée. Cordialement."
+  }] 
   
