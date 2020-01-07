44927d6646c6cb74d4ff81c0ea8cf261246a64d9
Claude Seguret <clodio@users.noreply.github.com>
2020-01-07 15:06:38 +0100

    suppression message

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,4 @@
  
 
-var welcome_page_messages = [{ "date" : "06/01/2020",
-    "message" : "Le service est temporairement indisponible, nous essayons de relancer le service dès que possible. Désolé pour la gêne occasionnée. Cordialement."
-  }] 
+var welcome_page_messages = [] 
   
