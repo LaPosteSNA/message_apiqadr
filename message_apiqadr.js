d0119c1356bf50794be3585d301b7f4221737fb7
LUDO33500 <ludovic.pigeon@laposte.fr>
2020-01-22 10:19:46 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,4 +1,5 @@
  
 
-var welcome_page_messages = [] 
+var welcome_page_messages = [[{ "date" : "23/01/2020",
+    "message" : "Une opération de maintenance est prévue ce jeudi en fin de matinée. Le service peut être interrompu quelques minutes pendant cette opération. Les fichiers de mois de 100 lignes risquent de ne pas être pris en compte, un correctif est en cours pour corriger ce problème. Nous sommes désolés pour la gêne occasionnée Cordialement."] 
   
