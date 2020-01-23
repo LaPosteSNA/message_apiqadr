939501ad85b347a779eeef2caa37721f694c5630
LUDO33500 <ludovic.pigeon@laposte.fr>
2020-01-23 11:11:27 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,4 @@
  
 
-var welcome_page_messages = [{ "date" : "22/01/2020",
- "message" : "Les fichiers de moins de 100 lignes risquent de ne pas être pris en compte, un correctif est en cours pour corriger ce problème. Déploiement du correctif prévu pour jeudi 23 Janvier en fin de matinée. Nous sommes désolés pour la gêne occasionnée."
-                             }]
+var welcome_page_messages = []
   
