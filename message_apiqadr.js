0611f62a8280defd31a9ab34f97f4163466ff42e
Claude Seguret <clodio@users.noreply.github.com>
2021-02-10 14:54:34 +0100

    Update message_apiqadr.js
    Ajout message pb emails

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,4 +1,6 @@
  
+var welcome_page_messages = [{ "date" : "10/02/2021",
+ "message" : "Pour une raison inconnue certains e-mails arrivent quelquefois dans le dossier indésirable de la messagerie outlook."
+}]
 
-var welcome_page_messages = []
   
