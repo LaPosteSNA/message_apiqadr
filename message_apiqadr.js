401e74c721d5f3b15dcc7dee74262af1e1eb953b
Claude Seguret <clodio@users.noreply.github.com>
2021-02-11 14:58:01 +0100

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,6 @@
  
-var welcome_page_messages = [{ "date" : "10/02/2021",
- "message" : "Pour une raison inconnue certains e-mails arrivent quelquefois dans le dossier indésirable de la messagerie outlook."
+var welcome_page_messages = [{ "date" : "11/02/2021",
+ "message" : "Suite à une modification de filtre outlook Microsoft, certains e-mails arrivent quelquefois dans le dossier indésirable de la messagerie outlook. Le point a été remonté, nous le suivons"
 }]
 
   
