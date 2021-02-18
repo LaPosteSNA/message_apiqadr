0777058152a1dedba8f0735c60b8ffa05a123104
Claude Seguret <clodio@users.noreply.github.com>
2021-02-18 11:19:00 +0100

    suppression message spam

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,4 @@
  
-var welcome_page_messages = [{ "date" : "12/02/2021",
- "message" : "Le problème d'e-mails arrivant quelquefois dans le dossier indésirable de la messagerie outlook a été corrigé par Microsoft, si cela survient à nouveau merci de nous prévenir"
-}]
+var welcome_page_messages = []
 
   
