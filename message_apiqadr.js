e4013308411e7b2b5551562fca4e706ab5d5478f
Claude Seguret <clodio@users.noreply.github.com>
2021-04-15 17:10:24 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,4 @@
  
-var welcome_page_messages = [{ "date" : "15/04/2021",
- "message" : "Depuis ce matin, un problème informatique général touche plusieurs services et notamment ce service de traitement d'adresse"
-}]
+var welcome_page_messages = []
 
   
